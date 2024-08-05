5f13390711cc4e5bc4ade08ff453d77b49af376b
Karam Guliyev <karamguliyev10@gmail.com>
2024-08-05 14:27:30 +0400

    Prisma Debugging

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -22,7 +22,6 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-<<<<<<< Updated upstream
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
       console.log("SignIn callback: ", { user, account, profile, email, credentials });
@@ -40,31 +39,6 @@ const authOptions: NextAuthOptions = {
       return baseUrl;
     },
   },
-=======
-
-  events: {
-    signIn: async (message: { user: User; account: Account | null; profile?: Profile }) => {
-      const { user, account, profile } = message;
-      console.log("User signed in:", user);
-      console.log("Account:", account);
-      console.log("Profile:", profile);
-    },
-  },
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      return true;
-    },
-    async redirect({ url, baseUrl }) {
-      return baseUrl;
-    },
-    async session({ session, user, token }) {
-      return session;
-    },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      return token;
-    },
-  },
->>>>>>> Stashed changes
 };
 
 const handler = NextAuth(authOptions);
